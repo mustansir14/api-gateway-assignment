ad2fd58f6240fa03c868c55feee9fdf9ac3ddd9b
Mustansir Muzaffar <mustansir2001@gmail.com>
2023-05-28 22:07:00 +0500

    added cors

--- API Gateway/main.py
+++ API Gateway/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
 from routes import auth, todos, news
@@ -7,6 +8,17 @@ load_dotenv()
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(news.router)
