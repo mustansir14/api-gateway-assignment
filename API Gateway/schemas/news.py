a1447507cdf1c3e169ae4e07249be31d4838dcae
Mustansir Muzaffar <mustansir2001@gmail.com>
2023-05-30 00:05:24 +0500

    remove gitignore files

--- API Gateway/schemas/news.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-from pydantic import BaseModel
-
-
-class ArticleSchema(BaseModel):
-
-    title: str
-    url: str
--- Microservice 3 (News)/src/main/java/dev/affan/newsapiddr/entity/Article.java
+++
@@ -1,9 +0,0 @@
-package dev.affan.newsapiddr.entity;
-
-import lombok.Data;
-
-@Data
-public class Article {
-    private String title;
-    private String url;
-}
